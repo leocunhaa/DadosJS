13423c6b34940fd4dae86cc106417d24abb210f6
leonardo <leomelo.py@gmail.com>
2023-10-26 11:11:54 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(express.json());
+
+// Conectar ao MongoDB usando o Mongoose
+mongoose.connect('mongodb://localhost/checklist', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Definir o modelo para a coleção 'task'
+const Task = mongoose.model('task', {
+  Nome: String,
+  Idade: Number
+});
+
+// Rota para adicionar uma nova tarefa
+app.post('/adicionar-tarefa', async (req, res) => {
+  try {
+    const { Nome, Idade } = req.body;
+    const novaTarefa = new Task({ Nome, Idade });
+    await novaTarefa.save();
+    res.json({ mensagem: 'Tarefa adicionada com sucesso!' });
+  } catch (error) {
+    res.status(500).json({ erro: 'Ocorreu um erro ao adicionar a tarefa.' });
+  }
+});
+
+// Iniciar o servidor
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
